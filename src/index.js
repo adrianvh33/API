52032f850438beb8dea559a498095b61913ac413
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-10-20 13:39:24 -0500

    interfaz de apollo

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const ProjectInvAPI = require('./dataSources/projectinv_api');
 
+const { ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} = require("apollo-server-core");
+
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV !== 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: "my-graph-id@my-graph-variant",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
 
   dataSources: () => ({
       projectInvAPI: new ProjectInvAPI(),
