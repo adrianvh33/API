976deec63088b686033f81ca7655bddc401a7bdd
adrianvh33 <adrian.valencia02@usc.edu.co>
2021-10-20 11:03:46 -0500

    se cambiar el servidos

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 module.exports ={
-    projectInv_api_url: process.env.PROJECTINV_API_URL || 'https://dw-equipo12.herokuapp.com/'
+    projectInv_api_url: process.env.PROJECTINV_API_URL || 'http://localhost:8080/'
 }
